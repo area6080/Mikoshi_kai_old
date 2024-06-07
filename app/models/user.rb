9b79279f55eae0bafa896af40906a1e89b37df01
Area6080 <area6080@gmail.com>
2024-06-07 08:19:27 +0000

    [Fix]header

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  validates :name, presence: true
+  
+  
   has_one_attached :profile_image
   
   def get_profile_image(width, height)
@@ -19,16 +22,3 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
-
-  # def get_profile_image(*size)
-  #   unless profile_image.attached?
-  #     file_path = Rails.root.join('app/assets/images/no-image.png')
-  #     profile_image.attach(io: File.open(file_path), filename: 'no-image.png', content_type: 'image/png')
-  #   end
-    
-  #   if !size.empty?
-  #     profile_image.variant(resize: size)
-  #   else
-  #     profile_image
-  #   end
-  # end
