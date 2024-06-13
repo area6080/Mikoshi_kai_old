ddd4969e6ba043d8d3bdb34a5f45e74e410c2819
Area6080 <area6080@gmail.com>
2024-06-13 02:45:56 +0000

    [Create]seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,49 @@ Admin.find_or_create_by!(email: ENV['ADMIN_EMAIL']) do |admin|
   admin.password = ENV['ADMIN_PASSWORD']
 end
 
-# = User.find_or_create_by!(email: "olivia@example.com") do |user|
-#   user.name = "Olivia"
-#   user.password = "password"
-#   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
-# end
 
-# PostEvent.find_or_create_by!(title: "Cavello") do |post_event|
-#   post_event.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
-#   post_event.caption = "大人気のカフェです。"
-#   post_event.address = "大阪府大阪市北区梅田１丁目" 
-#   post_event.user.name = 'olivia'
-# end
+onihei = User.find_or_create_by!(email: "aaa@aaa") do |user|
+  user.name = "長谷川　平蔵"
+  user.introduction = "＠鬼平でSNSに登録しています。follow me!"
+  user.password = "aaaaaa"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/onihei.jpg"), filename:"onihei.jpg")
+end
 
-# PostEvent.find_or_create_by!(title: "じゆうきままに") do |post_event|
-#   post_event.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
-#   post_event.caption = "小泉花陽推しです"
-#   post_event.address = "大阪府大阪市中央区心斎橋筋２丁目2-10　新日本三ツ寺ビル" 
-#   post_event.user.name = 'tatsuji'
-# end
+fishman = User.find_or_create_by!(email: "zzz@zzz") do |user|
+  user.name = "鮪　一番星"
+  user.introduction = "豊洲で海鮮料理店を経営しています。"
+  user.password = "zzzzzz"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/fish_man.jpg"), filename:"fish_man.jpg")
+end
 
-# PostEvent.find_or_create_by!(title: "ShoreditchBar") do |post_event|
-#   post_event.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
-#   post_event.caption = 'メキシコ料理好きな方にオススメ！'
-#   post_event.address = "大阪府大阪市淀川区西中島5-16-1" 
-#   post_event.user.name = 'lucas'
-# end
+galileo = User.find_or_create_by!(email: "sss@sss") do |user|
+  user.name = "ガリレオ"
+  user.introduction = "日本の文化が好きで移住しました。もう500年ほど前です。"
+  user.password = "ssssss"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/galileo.jpg"), filename:"galileo.jpg")
+end
+
+
+PostEvent.find_or_create_by!(title: "海上神輿") do |post_event|
+  post_event.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/torii.jpg"), filename:"torii.jpg")
+  post_event.user = onihei
+  post_event.caption = "左足が沈む前に右足を出しましょう！"
+  post_event.event_date = "2024/08/01"
+  post_event.address = "広島県廿日市市宮島町" 
+end
+
+PostEvent.find_or_create_by!(title: "超神田フェステイバル") do |post_event|
+  post_event.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/kanda.jpg"), filename:"kanda.jpg")
+  post_event.user = fishman
+  post_event.caption = "長年続いている伝統行事が今年も開催されます"
+  post_event.event_date = "2024/08/01"
+  post_event.address = "東京都千代田区外神田２丁目" 
+end
+
+PostEvent.find_or_create_by!(title: "なんとかだんじり祭り") do |post_event|
+  post_event.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/danjiri.jpg"), filename:"danjiri.jpg")
+  post_event.user = galileo
+  post_event.caption = "最高時速は300kmを超えるところが見どころ！"
+  post_event.event_date = "2024/09/01"
+  post_event.address = "大阪府岸和田市岸城町" 
+end
