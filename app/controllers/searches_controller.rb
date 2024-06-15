d798e8eedef30633b7a515b2bf994d82b4cb1464
Area6080 <area6080@gmail.com>
2024-06-15 03:07:20 +0000

    [Create]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,12 @@
 class SearchesController < ApplicationController
   def index
+    @model = params[:model]
+    @word = params[:word]
+
+    if @model == "User"
+      @result = User.looks(params[:word])
+    else
+      @result = PostEvent.looks(params[:word])
+    end
   end
 end
