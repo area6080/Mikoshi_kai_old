ec1ccf69e2498c060b9d0de689e4d23a973d2cb3
Area6080 <area6080@gmail.com>
2024-06-26 04:26:57 +0000

    [Fix]layouts

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -2,7 +2,7 @@ class Admin::GroupsController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @groups = Group.preload(:user)
+    @groups = Group.all
   end
 
   def show
