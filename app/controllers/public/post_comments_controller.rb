2d90e9a635703e8804cb2969c5a8dd50aab8d6e8
Area6080 <area6080@gmail.com>
2024-06-27 03:31:58 +0000

    [Fix]ポートフォリオ完了

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -5,8 +5,13 @@ class Public::PostCommentsController < ApplicationController
     post_event = PostEvent.find(params[:post_event_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_event_id = post_event.id
-    comment.save
-    redirect_to post_event_path(post_event)
+    if comment.save
+      flash[:notice] = "コメントを投稿しました!"
+      redirect_to post_event_path(post_event)
+    else
+      flash[:error] = comment.errors.full_messages
+      redirect_to request.referer
+    end
   end
 
   def destroy
