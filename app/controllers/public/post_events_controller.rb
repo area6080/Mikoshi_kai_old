47316763fe384699665d99b4df5d058decb89457
Area6080 <area6080@gmail.com>
2024-07-10 06:55:19 +0000

    [Fix]Controller

--- app/controllers/public/post_events_controller.rb
+++ app/controllers/public/post_events_controller.rb
@@ -25,23 +25,7 @@ class Public::PostEventsController < ApplicationController
     # 上記二行で制御せずreverse_eachを使用する
   end
 
-  # def create
-  #   @post_event = PostEvent.new(post_event_params)
-  #   @post_event.user_id = current_user.id
-  
-  #   if @post_event.image.attached?
-  #     @post_event.create_tag
-  #   end
-  
-  #   if @post_event.save
-  #     flash[:notice] = "イベントを投稿しました!"
-  #     redirect_to post_event_path(@post_event.id)
-  #   else
-  #     flash[:error] = @post_event.errors.full_messages
-  #     redirect_to request.referer
-  #   end
-  # end
-# FATコントローラ解決＆update対応させたいので検討中　うまく動かない
+
   def create
     @post_event = PostEvent.new(post_event_params)
     @post_event.user_id = current_user.id
@@ -82,7 +66,6 @@ class Public::PostEventsController < ApplicationController
     else
       flash[:error] = @post_event.errors.full_messages
       # エラーメッセージをflashに全部入れてしまっているのでリスト化できていない
-      # flash.now[:error] = @post_event.errors.full_messages
       render :edit
     end
   end
@@ -94,20 +77,39 @@ class Public::PostEventsController < ApplicationController
 
 
   private
-    def post_event_params
-      params.require(:post_event).permit(:title, :caption, :event_date, :address, :latitude, :longitude, :user_id, :image)
-    end
+  
+  def post_event_params
+    params.require(:post_event).permit(:title, :caption, :event_date, :address, :latitude, :longitude, :user_id, :image)
+  end
 
-    def update_post_event_params
-      params.require(:post_event).permit(:title, :caption, :event_date, :address, :latitude, :longitude, :image)
-    end
+  def update_post_event_params
+    params.require(:post_event).permit(:title, :caption, :event_date, :address, :latitude, :longitude, :image)
+  end
 
-    def is_matching_login_user
-      post_event = PostEvent.find(params[:id])
-      user = User.find(post_event.user_id)
-      unless user.id == current_user.id
-        redirect_to post_events_path
-      end
+  def is_matching_login_user
+    post_event = PostEvent.find(params[:id])
+    user = User.find(post_event.user_id)
+    unless user.id == current_user.id
+      redirect_to post_events_path
     end
+  end
   # 他人の投稿編集画面に入るのを無効化
 end
+
+  # def create
+  #   @post_event = PostEvent.new(post_event_params)
+  #   @post_event.user_id = current_user.id
+  
+  #   if @post_event.image.attached?
+  #     @post_event.create_tag
+  #   end
+  
+  #   if @post_event.save
+  #     flash[:notice] = "イベントを投稿しました!"
+  #     redirect_to post_event_path(@post_event.id)
+  #   else
+  #     flash[:error] = @post_event.errors.full_messages
+  #     redirect_to request.referer
+  #   end
+  # end
+# FATコントローラ解決＆update対応させたいので検討中　うまく動かない
