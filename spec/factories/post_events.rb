52d044fa953b807b16d9e80874748c382cff34ff
Area6080 <area6080@gmail.com>
2024-07-13 02:40:28 +0000

    [Rspec]create_Model

---
+++ spec/factories/post_events.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post_event do
+    title { Faker::Lorem.characters(number: 6) }
+    event_date { Faker::Lorem.characters(number: 8) }
+    address { Faker::Lorem.characters(number: 20) }
+    user
+  end
+end
